c0e98df473fcae804dcfd2709acd0971154865b3
Jeff Tao <jatao88@gmail.com>
2017-12-17 00:55:27 -0800

    Day 15 - cleanup

--- 15.py
+++ 15.py
@@ -1,3 +1,4 @@
+from itertools import islice
 a = 703
 b = 516
 af = 16807
@@ -9,39 +10,30 @@ def seq(x, f):
         x = (x * f) % div
         yield x
 
-def take(g, n):
-    return (v for v, _ in zip(g, range(n)))
-
 def lower(x):
     return x & 0xFFFF
 
+def count_lower_match(seq_a, seq_b, n):
+    print(sum(
+        # sum of booleans gives us an integer count of True
+        lower(ap) == lower(bp)
+        for ap, bp in
+            islice(
+                # zip the two generators to iterate over them simultaneously
+                zip(seq_a, seq_b),
+                n
+            )
+        )
+    )
+
 # Part 1: Number of pairs with matching lower 16 bits out of 40,000,000 pairs
-print(sum(
-    # sum of booleans gives us an integer count of True
-    lower(ap) == lower(bp)
-    for ap, bp in
-        # take restricts the number of pairs we generate
-        take(
-            # zip the two generators to iterate over them simultaneously
-            zip(seq(a, af), seq(b, bf)),
-            40000000
-        )
-    )
-)
+count_lower_match(seq(a, af), seq(b, bf), 40000000)
 
-
-# Part 1: Number of pairs with matching lower 16 bits out of 5,000,000 pairs in filtered sequences
-print(sum(
-    lower(ap) == lower(bp)
-    for ap, bp in
-        take(
-            zip(
-                # Same deal as before, but now each generator is further filtered
-                filter(lambda v: v % 4 == 0, seq(a, af)),
-                filter(lambda v: v % 8 == 0, seq(b, bf))
-            ),
-            5000000
-        )
-    )
+# Part 2: Number of pairs with matching lower 16 bits out of 5,000,000 pairs in filtered sequences
+count_lower_match(
+    # Same deal as before, but now each generator is further filtered
+    filter(lambda v: v % 4 == 0, seq(a, af)),
+    filter(lambda v: v % 8 == 0, seq(b, bf)),
+    5000000
 )
 
